c8d5995cbd608bf24b86f1a79d372d73fbe490db
alejandrobarranco <alejandro.barranco@geoenlace.com>
2019-08-12 11:30:53 -0500

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Alejandro Barranco on 12/08/19.
+//  Copyright © 2019 Alejandro Barranco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
