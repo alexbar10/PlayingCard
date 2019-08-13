a747ada59bd7de4d2ea1d893eb216339b0909ce8
alejandrobarranco <alejandro.barranco@geoenlace.com>
2019-08-13 14:18:32 -0500

    chore(): Lecture 6

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class PlayingCardView: UIView {
     
-    var rank: Int = 8 { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+    @IBInspectable
+    var rank: Int = 12 { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+    @IBInspectable
     var suit: String = "♥︎"  { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+    @IBInspectable
     var isFaceUp: Bool = false { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+    
+    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBoundsSize { didSet { setNeedsDisplay() } }
+    
+    @objc func adjustFaceCardScale(byHandlingGesrtureRecognizedBy recognizer: UIPinchGestureRecognizer) {
+        switch recognizer.state {
+        case .changed, .ended:
+            faceCardScale *= recognizer.scale
+            recognizer.scale = 1.0
+        default:
+            break
+        }
+    }
     
     private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
@@ -126,19 +142,20 @@ class PlayingCardView: UIView {
 //        path.stroke()
 //        path.fill()
         
+        /// drawing a card
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         roundedRect.addClip()
         UIColor.white.setFill()
         roundedRect.fill()
         
         if isFaceUp {
-            if let faceCardImage = UIImage(named: rankString+suit) {
-                faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+            if let faceCardImage = UIImage(named: rankString+suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
+                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
             } else {
                 drawPips()
             }
         } else {
-            if let cardBackImage = UIImage(named: "cardback") {
+            if let cardBackImage = UIImage(named: "cardback", in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
                 cardBackImage.draw(in: bounds)
             }
         }
--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -12,15 +12,37 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipeGesture.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipeGesture)
+            
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGesrtureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
     
+    /// MARK: Target Action
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+    
+    /// MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        for _ in 0...10 {
-            if let card = deck.draw() {
-                print("Card: \(card)")
-            }
-        }
     }
 }
